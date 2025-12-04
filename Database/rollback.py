5be8711261aed60dd498fa1b92a4d392aeb10bb7
kuldeep sharma <mekuldeep03@gmail.com>
2025-12-04 23:53:02 +0530

    ain

--- Database/rollback.py
+++ Database/rollback.py
@@ -1,1 +1,17 @@
-# rollback is maybe use for revert the query 
+# rollback is maybe use for revert the query import mysql.connector
+
+connection = mysql.connector.connect(...)
+
+with connection.cursor() as cursor:
+    try:
+        cursor.execute("INSERT INTO test (name) VALUES ('A')")
+        cursor.execute("INSERT INTO test (name) VALUES ('B')")
+        connection.commit()
+    except:
+        connection.rollback()
+
+
+
+# hat is a Transaction?./3
+
+# A transaction is a group of SQL operations that are exe
