e376810c24e5fd0fa128d8c710458bcdbb783762
kuldeep sharma <mekuldeep03@gmail.com>
2026-02-05 23:59:31 +0530

    unit1

---
+++ MSc/unit1.py
@@ -0,0 +1,2 @@
+# Basics: Indentation, comments, importing a module, standard scalar data types, Control flow:
+# if-else statements, loops (while, for), pass, range; Lists, Tuples, Sets, Dictionaries.
