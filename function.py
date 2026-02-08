46bf84dec738a215c417f526090f1190ef752a5c
kuldeep sharma <mekuldeep03@gmail.com>
2026-02-08 12:58:45 +0530

    lambda functions

--- function.py
+++ function.py
@@ -3,5 +3,15 @@ def getLength(list):
 
 cities = ['bikaner', 'jaipur', 'jodhpur', 'jaisalmer']
 
-# recursion
+## Lambda function
+## Example 1
+# sum = lambda a,b: a+b
+# print(sum(2,4))
+
+
+## Example 2
+# nums = [1,2,3,4,5]
+# mul = list(map(lambda x: x*2, nums))
+
+# print(add)
 
