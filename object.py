70e2e30d12a60fd23da0945400a9289235338869
kuldeep sharma <mekuldeep03@gmail.com>
2026-02-09 23:25:55 +0530

    objects

---
+++ object.py
@@ -0,0 +1,20 @@
+"""
+What is an Object?
+
+Everything in Python is an object.
+
+Examples:
+x = 10        # int object
+name = "Kuldeep"  # string object
+nums = [1,2,3]    # list object
+
+
+Python is object-oriented language.
+An object contains:
+Data (attributes)
+Functions (methods)
+"""
+
+
+x = 10
+print(dir(x))
