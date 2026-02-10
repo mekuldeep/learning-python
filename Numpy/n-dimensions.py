16b607e900e2090827211cffe9ac7bf664227f85
kuldeep sharma <mekuldeep03@gmail.com>
2026-02-10 23:46:39 +0530

    numpy intro

---
+++ Numpy/n-dimensions.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+# Create array from Python list → np.array()
+
+# 1D array
+arr = np.array([1,2,3,4,5])
+# print(type(arr))
+
+#2D Array
+arr2 = np.array([[1,2,3], [4,5,6]])
+# print(arr2)
+
+# 3D Array
+arr3 = np.array([
+                    [[1,2,3], [4,5,6]],
+                    [[7,8,9], [0,4,5]]
+                ])
+
+# print(arr3)
+
+
+"""
+Basically 1D = use 1 square bracket
+2D = use 2 square brackets
+3D = use 3 square brackets
+""" 
+
+## Property	Meaning
+# ndim	number of dimensions
+# shape	size of array
+# size	total elements
+
+
+print(arr3.ndim)
+print(arr3.shape)
+print(arr3.size)
