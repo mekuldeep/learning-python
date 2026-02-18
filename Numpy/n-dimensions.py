5fa0388bfac2841ad3616fb6f4a810f89018844c
kuldeep sharma <mekuldeep03@gmail.com>
2026-02-18 20:19:43 +0530

    learn numpy

--- Numpy/n-dimensions.py
+++ Numpy/n-dimensions.py
@@ -60,9 +60,13 @@ Basically 1D = use 1 square bracket
 
 # print(arr[:4])
 
-arr = np.array([1, 2, 3, 4])
+# arr = np.array([1, 2, 3, 4])
 
-print(arr[2] + arr[3])
+# print(arr[2] + arr[3])
+
+arr = np.array([[1,2,3,4,5], [6,7,8,9,10]])
+
+print('2nd element on 1st row: ', arr[0, 1])
 
 
 
