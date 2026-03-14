52d2e703167d49438f997196138347f4a93b861b
kuldeep sharma <mekuldeep03@gmail.com>
2026-03-14 23:27:34 +0530

    learn python

--- Numpy/n-dimensions.py
+++ Numpy/n-dimensions.py
@@ -6,11 +6,11 @@ import numpy as np
 arr = np.array([1,2,3,4,5])
 # print(type(arr))
 
-#2D Array
+#2D Array using numpy
 arr2 = np.array([[1,2,3], [4,5,6]])
 # print(arr2)
 
-# 3D Array
+# 3D Array numpy
 arr3 = np.array([
                     [[1,2,3], [4,5,6]],
                     [[7,8,9], [0,4,5]]
