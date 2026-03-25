11c72ae82a81d2ea302d8c87a1303d453ea5f537
kuldeep sharma <mekuldeep03@gmail.com>
2026-03-25 21:59:02 +0530

    file handling

--- conditional.py
+++ conditional.py
@@ -13,16 +13,16 @@
 #     print(" This is odd number ")
 
 # 2 practice question
-num1 = int(input("Enter 1st Number : "))
-num2 = int(input("Enter 2nd Number : "))
-num3 = int(input("Enter 3rd Number : "))
+# num1 = int(input("Enter 1st Number : "))
+# num2 = int(input("Enter 2nd Number : "))
+# num3 = int(input("Enter 3rd Number : "))
 
-if (num1 > num2 and num1 > num3):
-    print(f"{num1} is biggest value")
-elif (num2 > num3) :
-    print(f"{num2} is biggest value")
-else:
-    print(f"{num3} is biggest value")    
+# if (num1 > num2 and num1 > num3):
+#     print(f"{num1} is biggest value")
+# elif (num2 > num3) :
+#     print(f"{num2} is biggest value")
+# else:
+#     print(f"{num3} is biggest value")    
 
 # 3 practice question
 # num = int(input('Enter a number : '))
